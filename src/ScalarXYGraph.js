9cfb32a5f98452b116a1250c15cce8d297882bc7
Ben Nortier <ben@bjnortier.com>
2018-07-25 13:48:02 +0200

    Add semi-transparent frame to y legend

--- src/ScalarXYGraph.js
+++ src/ScalarXYGraph.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { max } from 'lodash'
 
 import ScalarXAxis from './ScalarXAxis'
 import ScalarYAxis from './ScalarYAxis'
@@ -19,6 +20,7 @@ class ScalarXYGraph extends Component {
     const xLayout = getLayout('x', [xMin, xMax], contentsWidth)
     const yLayout = getLayout('y', [yMin, yMax], contentsHeight)
     const layout = {x: xLayout, y: yLayout}
+    const maxYLabelLength = max(data.y.map(y => y.label.length))
     // The entire graph is offset by 0.5,0.5 pixesl to get crisp single
     // pixel lines
     // https://kilianvalkhof.com/2010/design/the-problem-with-svg-and-canvas/
@@ -27,20 +29,6 @@ class ScalarXYGraph extends Component {
         <text style={{textAnchor: 'middle'}} x={padding + contentsWidth / 2} y={padding / 2 + 10} >
           {title}
         </text>
-        {data.y.map((y, i) => {
-          const r1 = 1.5
-          const r2 = 3
-          return <g key={i} transform={`translate(${padding}, ${padding + i * 20})`}>
-            <line stroke={colors[i]} x1={3} x2={17} y1={10} y2={10} />
-            <g transform={`translate(10, 10)`}>
-              <circle stroke='none' x={5} y={5} fill={colors[i]} r={r2} />
-              <circle stroke='none' fill='white' r={r1} />
-            </g>
-            <text style={{textAnchor: 'left'}} x={20} y={14} >
-              {y.label}
-            </text>
-          </g>
-        })}
         <g transform={`translate(${padding}, ${height - padding})`}>
           <ScalarXAxis
             width={width - padding * 2}
@@ -64,6 +52,23 @@ class ScalarXYGraph extends Component {
             colors={colors}
             data={data}
           />
+        </g>
+        <g transform={`translate(${padding}, ${padding})`}>
+          <rect x={0} y={0} width={20 + maxYLabelLength * 7.5 + 10} height={data.y.length * 20} fill='#fff' stroke='#ddd' fillOpacity={0.5} />
+          {data.y.map((y, i) => {
+            const r1 = 1.5
+            const r2 = 3
+            return <g key={i} transform={`translate(0, ${i * 20})`}>
+              <line stroke={colors[i]} x1={3} x2={17} y1={10} y2={10} />
+              <g transform={`translate(10, 10)`}>
+                <circle stroke='none' x={5} y={5} fill={colors[i]} r={r2} />
+                <circle stroke='none' fill='white' r={r1} />
+              </g>
+              <text style={{textAnchor: 'left'}} x={20} y={14} >
+                {y.label}
+              </text>
+            </g>
+          })}
         </g>
       </g>
     </svg>

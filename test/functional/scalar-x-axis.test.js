7939e0bc5b02583bc300b302e86f50ef0592e0ad
Ben Nortier <ben@bjnortier.com>
2018-12-10 16:22:37 +0200

    Upgrade babel & refactor functional tests

---
+++ test/functional/scalar-x-axis.test.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import getLayout from '../../src/getLayout'
+import { ScalarXAxis } from '../../src'
+
+const examples = [
+  [],
+  [10],
+  [0, 1],
+  [0, 0.1],
+  [0, 10],
+  [0, 11],
+  [0, 50],
+  [0, 100],
+  [1, 11],
+  [0, 4.5],
+  [0, 9.9],
+  [-2, 10],
+  [16, 28],
+  [-40, 7],
+  [1000, 2000],
+  [1000, 2001],
+  [1000, 1999],
+  [-1000, -20],
+  [-10000, 10000],
+  [-100000, 1000000],
+  [0, 0.01],
+  [0, 0.001]
+]
+const widths = [800, 640, 480, 320, 240]
+const height = 50
+const padding = 25
+
+const Limits = styled.div`
+  margin: 20px;
+`
+
+const Width = styled.div`
+  display: inline-block;
+  margin: 10px;
+`
+
+const SVGContainer = styled.div`
+  width: ${({ width }) => width}px;
+  margin: 20px;
+  background-color: #fff;
+`
+
+export default (props) => <div>
+  {examples.map((limits, i) => {
+    return <div key={i}>
+      <Limits>limits={JSON.stringify(limits)}</Limits>
+      {widths.map(width => {
+        const layout = getLayout('x', limits, width)
+        return <SVGContainer
+          key={width}
+          width={width + padding * 2}
+        >
+          <Width>{width}px</Width>
+          <svg width={width + padding * 2} height={height}>
+            <g transform={`translate(${padding}, 0)`}>
+              <line stroke='#ddd' x1={0} y1={0} x2={width} y2={0} />
+              <ScalarXAxis
+                width={width}
+                height={height}
+                layout={layout}
+                label='X'
+              />
+            </g>
+          </svg>
+        </SVGContainer>
+      })}
+    </div>
+  })}
+</div>
---
+++ test/functional/scalar-y-axis.test.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import getLayout from '../../src/getLayout'
+import { ScalarYAxis } from '../../src'
+
+const examples = [
+  [0, 10],
+  [],
+  [0],
+  [10],
+  [0, 1],
+  [0, 0.1],
+  [0, 11],
+  [0, 50],
+  [0, 100],
+  [1, 11],
+  [0, 4.5],
+  [0, 9.9],
+  [-2, 10],
+  [16, 28],
+  [-40, 7],
+  [1000, 2000],
+  [1000, 2001],
+  [1000, 1999],
+  [-1000, -20],
+  [-10000, 10000],
+  [-100000, 1000000],
+  [0, 0.01],
+  [0, 0.001]
+]
+
+const heights = [320, 240, 180, 120, 80]
+const width = 50
+const padding = 50
+
+const Limits = styled.div`
+  margin: 20px;
+`
+
+const Height = styled.div`
+  margin-left: 20px;
+  padding: 10px;
+  padding-bottom: 0;
+  width: ${({ width }) => width - 20}px;
+  background-color: #fff;
+`
+
+const SVGContainer = styled.div`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  margin: 20px;
+  margin-top: 0;
+  background-color: #fff;
+`
+
+export default (props) => <div>
+  {examples.map((limits, i) => {
+    return <div key={i}>
+      <Limits>limits={JSON.stringify(limits)}</Limits>
+      {heights.map(height => {
+        const layout = getLayout('y', limits, height)
+        return <React.Fragment key={height}>
+          <Height width={width + padding}>{height}</Height>
+          <SVGContainer
+            width={width + padding}
+            height={height + padding * 2}
+          >
+            <svg width={width} height={height + padding * 2}>
+              <g transform={`translate(0, ${padding})`}>
+                <line stroke='#ddd' x1={width} y1={0} x2={width} y2={height} />
+                <ScalarYAxis
+                  width={width}
+                  height={height}
+                  layout={layout}
+                />
+              </g>
+            </svg>
+          </SVGContainer>
+        </React.Fragment>
+      })}
+    </div>
+  })}
+</div>

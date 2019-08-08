fffb1f5b9cb403054f7c5bb426ad8b146bde4415
Ben Nortier <ben@bjnortier.com>
2019-08-08 10:03:58 +0200

    Add TimeXAgregateY to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ScalarXAxis from './ScalarXAxis'
 import ScalarYAxis from './ScalarYAxis'
 import ScalarXScalarYGraph from './ScalarXScalarYGraph'
 import TimeXScalarYGraph from './TimeXScalarYGraph'
+import TimeXAggregateYGraph from './TimeXAggregateYGraph'
 import Sparkline from './Sparkline'
 import colors10 from './util/colors10'
 
@@ -12,6 +13,7 @@ export {
   ScalarYAxis,
   ScalarXScalarYGraph,
   TimeXScalarYGraph,
+  TimeXAggregateYGraph,
   Sparkline,
   colors10
 }

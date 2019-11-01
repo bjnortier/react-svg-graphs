143204302d6ac3560b0beb3f89cd009704c1d1fa
Ben Nortier <ben@bjnortier.com>
2019-11-01 11:00:57 +0200

    new cat colors

--- src/util/colors10.js
+++ src/util/colors10.js
@@ -1,14 +1,14 @@
 // Adapted from https://github.com/d3/d3-scale-chromatic/blob/master/src/categorical/category10.js
 
 export default [
-  '#1f77b4',
-  '#2ca02c',
-  '#d62728',
-  '#ff7f0e',
-  '#9467bd',
-  '#8c564b',
-  '#e377c2',
-  '#7f7f7f',
-  '#bcbd22',
-  '#17becf'
+  '#4e79a7',
+  '#59a14f',
+  '#76b7b2',
+  '#edc948',
+  '#b07aa1',
+  '#9c755f',
+  '#f28e2b',
+  '#aaa0ac',
+  '#ff9da7',
+  '#e16759'
 ]
